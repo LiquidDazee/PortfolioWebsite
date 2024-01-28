a34b78cc65924639afd8063438b8ba5607eabb11
LiquidDazee <neelakg1999@gmail.com>
2024-01-28 21:33:33 +0530

    Updated viteconfig again

--- portfolio-site/vite.config.ts
+++ portfolio-site/vite.config.ts
@@ -5,13 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-  port: 5173,
+  port: 5174,
   strictPort: true,
  },
   server: {
-  port: 5173,
+  port: 5174,
   strictPort: true,
   host: true,
-  origin: "http://0.0.0.0:5173",
+  origin: "http://0.0.0.0:5174",
  },
 });
